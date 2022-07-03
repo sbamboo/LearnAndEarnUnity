5e115681dfd23bef2d925ce73789adac9834285f
simonkalmiclaesson <simon.claesson8@gmail.com>
2022-07-03 14:49:33 +0200

    Added CammeraFollow

--- Assets/Code/Player/PlayerController2.cs
+++ Assets/Code/Player/PlayerController2.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerController2 : MonoBehaviour
 {
-    private Rigidbody2D rb2D;
+    // Script Setup
+    public Rigidbody2D rb2D;
+    public float movespeed = 3f;
+    public float jumpforce = 40f;
+    public bool isjumping = false;
 
-    private float movespeed;
-    private float jumpforce;
-    private bool isjumping;
+    // Define moves
     private float moveHorizontal;
     private float moveVertical;
 
@@ -16,18 +18,15 @@ public class PlayerController2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb2D = gameObject.GetComponent<Rigidbody2D>();
-
-        movespeed = 3f;
-        jumpforce = 40f;
-        isjumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get Horizontal movement
         moveHorizontal = Input.GetAxisRaw("Horizontal");
        
+        //Jumping
         if (Input.GetKey(KeyCode.Space))
         {
             moveVertical = 1;
@@ -37,43 +36,37 @@ public class PlayerController2 : MonoBehaviour
             moveVertical = Input.GetAxisRaw("Vertical");
          
         }
-        // Debug.Log(Input.GetAxisRaw("Vertical"));
     }
+
+    //Fixed update
     void FixedUpdate()
     {
+        //Move character horizontaly (Apply moveSpeed when inputs are given for horizontal movement)
         if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
         {
             rb2D.AddForce(new Vector2(moveHorizontal * movespeed, 0f), ForceMode2D.Impulse);
-
-
         }
+
+        //Move character vericaly (Jump if character is not already jumping and a veritcal input is given)
         if (!isjumping && moveVertical > 0.1f)
         {
             rb2D.AddForce(new Vector2(0f, moveVertical * jumpforce), ForceMode2D.Impulse);
         }
 
-
-
-
-
+        //Rotate Sprite Right
         if (moveHorizontal > 0)
         {
             gameObject.transform.localScale = new Vector3(3, 4, 1);
         }
 
+        //Rotate Sprite Left
         if (moveHorizontal < 0)
         {
             gameObject.transform.localScale = new Vector3(-3, 4, 1);
         }
-
-
-
-
-
-
-
     }
 
+    //Only allow jumping on a collider/object tagged "platform"
     void OnTriggerEnter2D(Collider2D collision) 
     {
         if(collision.gameObject.tag == "Platform")
